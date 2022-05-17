2c3bba13620f9fe1bcc587d87bbb4eaea036e3c1
akshayk-github <laxmivaghasiya134@gmail.com>
2022-05-17 10:56:40 +0530

    Projact Start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header';
+import Home from './container/Home/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  return (
-    <>
-    
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Home />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
